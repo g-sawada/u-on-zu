016b293f600b9709f9ccfd1be1b0156b66631401
g.sawada <sawada.gota@gmail.com>
2024-05-15 16:45:11 +0900

    usersコントローラー・newビューの作成とルーティング簡易設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  # ユーザーの新規作成・削除を担う。マイページの表示と更新はprofileコントローラーで行う
+
+  # 新規作成画面の表示
+  def new
+  end
+
+  # 新規登録処理
+  def create
+  end
+
+  # ユーザー削除処理
+  def destroy
+  end
+end
