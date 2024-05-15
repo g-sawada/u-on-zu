24439622603faa67ca6903dcf4b373694f49d272
g.sawada <sawada.gota@gmail.com>
2024-05-15 10:02:23 +0900

    自動生成ファイルをslimに変更。tailwindでslimを読み込むように設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.generators.template_engine = :slim # テンプレートエンジンをslimに設定
+    
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
