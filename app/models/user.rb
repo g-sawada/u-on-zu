44d744dbf75cd26e17ef7c056fd90f9fb077cfff
g.sawada <sawada.gota@gmail.com>
2024-05-15 23:48:06 +0900

    Userモデルのenum変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
   validates :email,                 presence: true, uniqueness: true, length: { maximum: 255 }
   validates :occupation,            presence: true
 
-  enum occupation: { 
-    high_geography: 0, high_social: 1, high_others: 2, junior_social: 3, junior_others: 4, elementary: 5, 
-    college_geography: 6, college_others: 7, other_teacher: 8, aspirant: 9, others: 10 }
+  enum occupation: {
+    student_elementary: 0, student_junior: 1, student_high: 2, student_college: 3, teacher_high_geography: 4,
+    teacher_high_social: 5, teacher_high_others: 6, teacher_junior_social: 7, teacher_junior_others: 8,
+    teacher_elementary: 9, researcher_geography: 10, researcher_others: 11, teacher_others: 12,
+    company_education: 13, company_others: 14, others: 15
+  }
 end
