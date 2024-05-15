f07808fc35d8162a731c73ad63eb0588f90dd11b
g.sawada <sawada.gota@gmail.com>
2024-05-15 16:23:24 +0900

    sorcery:install の実行

---
+++ db/migrate/20240515065859_sorcery_core.rb
@@ -0,0 +1,13 @@
+class SorceryCore < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :email,            null: false, index: { unique: true }
+      t.string :crypted_password
+      t.string :salt
+      t.string :username,         null: false
+      t.integer :occupation
+
+      t.timestamps                null: false
+    end
+  end
+end
