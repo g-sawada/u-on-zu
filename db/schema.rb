42064ccf5c7579353692be6c4c4002cd58add9b1
g.sawada <sawada.gota@gmail.com>
2024-05-15 16:32:26 +0900

    Userモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_065859) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "username", null: false
+    t.integer "occupation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
