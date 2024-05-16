dca5cdaca4c7e606c7d57c22d6f3018f6052635a
g.sawada <sawada.gota@gmail.com>
2024-05-17 00:14:16 +0900

    canvas/componentsディレクトリにgraph.jsxを作成。グラフの描画と簡易レイアウト実装

--- app/javascript/react/canvas/index.jsx
+++ app/javascript/react/canvas/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Graph from './components/graph';
 
 export default function CanvasApp() {
   return (
-    <div className="text-2xl m-10">
-      <h1>My Canvas App</h1>
-      <p>It works!</p>
+    <div className='flex'>
+      <div className='flex flex-1 justify-center items-center bg-blue-200'>
+        <div className='mt-10'>
+          <Graph />
+        </div>
+      </div>
+      <div className='w-full flex-shrink-0 max-w-xs bg-green-200 '>
+        テスト
+      </div>
     </div>
   )
 }
