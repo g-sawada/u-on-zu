84d2afad324664a441a5b9b206a3e296ec5e0e5a
g.sawada <sawada.gota@gmail.com>
2024-05-16 19:42:46 +0900

    CanvasAppの描画テスト完了

---
+++ app/javascript/react/canvas/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CanvasApp() {
+  return (
+    <div className="text-2xl m-10">
+      <h1>My Canvas App</h1>
+      <p>It works!</p>
+    </div>
+  )
+}
--- app/javascript/react/entrypoints/canvas_entry.jsx
+++ app/javascript/react/entrypoints/canvas_entry.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import CanvasApp from '../canvas';
 
 document.addEventListener('turbo:load', () => {
-  const container = document.getElementById('create-graph-app');
+  const container = document.getElementById('canvas-app');
   if (container) {
-  createRoot(container).render(<Test />);
+  createRoot(container).render(<CanvasApp />);
   } else {
-  console.log('test_app not found');
+  console.log('canvas-app not found');
   }
 })
