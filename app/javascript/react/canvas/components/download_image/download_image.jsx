ba36519c32f064cec3618183c42aa994017e0845
g.sawada <sawada.gota@gmail.com>
2024-05-18 10:03:18 +0900

    DownloadImageコンポーネントの作成

---
+++ app/javascript/react/canvas/components/download_image/download_image.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DownloadImage() {
+  return(
+    <div className='my-5'>
+      <button className='btn btn-primary'>
+        DL Image
+      </button>
+    </div>
+  )
+}
