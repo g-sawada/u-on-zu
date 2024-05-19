7bc47b54b80c2e2a9078cb8e647ffaefd7fbad94
g.sawada <sawada.gota@gmail.com>
2024-05-19 11:54:11 +0900

    api/graphsコントローラーにshowアクションをテスト実装

--- app/controllers/api/graphs_controller.rb
+++ app/controllers/api/graphs_controller.rb
@@ -1,5 +1,14 @@
 class Api::GraphsController < Api::BaseController
+  # GET /api/graphs
+  # テスト表示用
   def index
-    render json: { message: 'Hello, World!' }
+    graphs = Graph.all
+    render json: { message: 'Hello, World!', graphs: graphs }
+  end
+
+  # GET /api/graphs/:id
+  def show
+    graph = Graph.find(params[:id])
+    render json: { graph: graph, graph_setting: graph.graph_setting }
   end
 end
