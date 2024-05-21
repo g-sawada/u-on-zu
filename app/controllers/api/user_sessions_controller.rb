e5dba8e38261df25856c990fa5accfea3e466aa8
g.sawada <sawada.gota@gmail.com>
2024-05-21 12:24:39 +0900

    ログイン状態を判定する処理と，ログイン中かつgraphパラメータがあるときにデータを更新するまでローディングする処理を追加

--- app/controllers/api/user_sessions_controller.rb
+++ app/controllers/api/user_sessions_controller.rb
@@ -1,5 +1,5 @@
 class Api::UserSessionsController < Api::BaseController
   def check_login
-    render json: { login: logged_in? }
+    render json: { logged_in: logged_in? }
   end
 end
--- app/javascript/react/canvas/hooks/useGraph.js
+++ app/javascript/react/canvas/hooks/useGraph.js
@@ -1,31 +1,68 @@
 import { useState, useEffect } from 'react';
 
-export const useGraph = (param) => {            //引数はgraphParamを受ける
-  const [graph, setGraph] = useState([]);       //初期値は空の配列
-  const [loading, setLoading] = useState(true); //fetch完了まで描画してはいけないのでloadingをtrueに
+export const useGraph = (
+  param,
+  loginCheckLoading,
+  loggedIn ) => {               //引数にgraphParamと，ログインチェック中状態，ログイン状態を受け取る
+  
+  const [graph, setGraph] = useState(null);        //初期値は空の配列
+  const [graphLoading, setGraphLoading] = useState(true);   //ここのローディング状態を管理するstate
 
   useEffect(() => {                             //useEffectはレンダリング完了時に実行される
-    async function fetchGraph(param) {          //ここからfetch関数を定義。
-      console.log('param: ', param)
-      try {
+    // console.log('param:', param, ' loginCheckLoading:', loginCheckLoading, ' loggedIn:', loggedIn);
+    
+    if (loginCheckLoading) {   
+      console.log('ログインチェック中のためreturnします')                 //ログインチェック中の場合は，何もせず即リターン
+      return
+    }
+
+    if (!loggedIn) {              //ログインしていない場合は，ここのローディング状態をfalseにするだけで即リターン
+      console.log('ログインしていないのでreturnします')
+      setGraphLoading(false);
+      return
+    }
+
+    if (!param) {     //ログインしているが，graphParamがnullの場合は，ここのローディング状態をfalseにするだけで即リターン
+      console.log('graphParamがnullなのでreturnします')
+      setGraphLoading(false);
+      return
+    }
+
+    async function fetchGraph(param) {         //ここからfetch関数を定義。
+      try{
         const response = await fetch(`/api/graphs/${param}`);   //api/graphs/:idにfetchリクエストを送る（showアクション）
         if (response.ok) {
           const graph = await response.json();
-          console.log(graph)
           setGraph(graph);  //取得したgraphデータをstateにセット
-          setLoading(false); //fetch完了したのでloadingをfalseに
-        } else {
-          console.log('server error!');
-          setLoading(false);  //fetch処理は完了したのでloadingをfalseに
+          // console.log('setしたgraph:', graph)
         }
       } catch (error) {
-          console.log('failed to fetch', error);
-          setLoading(false);
+        console.log('failed to fetch', error);
+      } finally {
+        setGraphLoading(false);  //fetch終了時にローディング状態をfalseに
       }
     }
-
+    // console.log('ここまできてる？')
+    // //ここまで来た場合は，ログインしていて，graphParamがnullでない場合
+    // async function fetchGraph(param) {         //ここからfetch関数を定義。
+    //   try {
+    //     const response = await fetch(`/api/graphs/${param}`);   //api/graphs/:idにfetchリクエストを送る（showアクション）
+    //     if (response.ok) {
+    //       const graph = await response.json();
+    //       setGraph(graph);  //取得したgraphデータをstateにセット
+    //       console.log('setしたgraph:', graph)
+    //     } else {
+    //       console.log('server error!');
+    //     }
+    //   } catch (error) {
+    //       console.log('failed to fetch', error);
+    //   } finally {
+    //     setGraphLoading(false);  //fetch終了時にローディング状態をfalseに
+    //   }
+    // }
+    // console.log("ここにもきてる？？")
     fetchGraph(param);    //上で定義したfetch関数を実行
-  }, []);                 //useEffectここまで。第二引数に空の配列を渡すことで、初回レンダリング時のみfetchを実行する
+  }, [param, loginCheckLoading, loggedIn]);            //paramとloginCheckLoadingが変更されたことを監視
 
-  return { graph, loading }   //fetchしたgraphデータとloadingのstateを返す
+  return { graph, graphLoading }   //fetchしたgraphデータを返す
 }
