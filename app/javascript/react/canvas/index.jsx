abdf0e993afb158dcc4d3100705ebefb6d4f1e91
g.sawada <sawada.gota@gmail.com>
2024-05-24 16:06:00 +0900

    マイグラフ一覧からcanvas遷移時に，紐づいている都市データの呼び出しと反映を実行

--- app/javascript/react/canvas/index.jsx
+++ app/javascript/react/canvas/index.jsx
@@ -148,6 +148,8 @@ export default function CanvasApp() {
       setSettingValues({...settingValues, title: city.name});   //グラフ設定値のタイトルの初期値を都市名に設定
     }
     if (graph && graph.graph_setting) {
+      console.log('graphのcity_id:', graph.graph.city_id)
+      setCityId(graph.graph.city_id);
       setSettingValues(graph.graph_setting.settings);
     }
   }, [graph, graphLoading, city, cityLoading]);
