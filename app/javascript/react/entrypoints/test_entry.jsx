628b49648287165e13da7da7d3a5881b59458814
g.sawada <sawada.gota@gmail.com>
2024-05-29 18:01:16 +0900

    onClickで都市名つきのInfoWindowを出現させるテスト実装

--- app/javascript/react/entrypoints/test_entry.jsx
+++ app/javascript/react/entrypoints/test_entry.jsx
@@ -5,26 +5,170 @@ import { StyledEngineProvider } from '@mui/material/styles';
 import { Button } from '@mui/material';
 
 // GoogleMapテスト
-import { GoogleMap, useLoadScript } from "@react-google-maps/api";
-import { Marker, InfoWindow } from "@react-google-maps/api";
+// import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+// import { Marker, InfoWindow } from "@react-google-maps/api";
 
-import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import {APIProvider,
+        Map,
+        Marker,
+        AdvancedMarker,
+        InfoWindow,
+        useAdvancedMarkerRef,
+        Pin} from '@vis.gl/react-google-maps';
 
 const googleMapsApiKey = gon.google_maps_api_key; //コントローラーでgonに読み込ませたAPIキーを取得
 
 function GoogleMapComponent() {
+  //モックデータ
+  const cityIdMapping = [
+    { id: 1, name: '東京', position: { lat: 35.6917, lng: 139.75, alt: 25.2 }},
+    { id: 2, name: '大阪', position: { lat: 34.6817, lng: 135.5183, alt: 23.0 }},
+    { id: 3, name: '札幌', position: { lat: 43.06, lng: 141.3283, alt: 17.4 }},
+    { id: 4, name: '那覇', position: { lat: 26.2067, lng: 127.6867, alt: 28.1 }},
+  ]
+  const [selected, setSelected] = useState(null);
+  const handleClose = () => {
+    setSelected(null);
+  }
+
+
   return (
     <APIProvider apiKey={googleMapsApiKey}>
       <Map
+        mapId={'160dcc337dc1872'}
         style={{width: '100%', height: '60vh'}}
         defaultCenter={{lat: 36.6513, lng: 138.1810}}
         defaultZoom={4.6}
         gestureHandling={'greedy'}
         disableDefaultUI={true}
-      />
+      >        
+        {cityIdMapping.map((city) => ( 
+          <MarkerWithInfoWindow
+            key={city.id}
+            city={city}
+            InfoWindowOpen={ selected === city.id }
+            onMarkerClick={() => {
+              setSelected(city.id)
+              console.log('city_id: ', city.id, 'city_name: ', city.name)
+            }}
+            setSelected={setSelected}
+            handleClose={handleClose}
+          />
+        ))}
+      </Map>
     </APIProvider>
   )
 }
+
+function MarkerWithInfoWindow({city, InfoWindowOpen, onMarkerClick, handleClose}) {
+  const [markerRef, marker] = useAdvancedMarkerRef();
+
+  return (
+    <>
+      <AdvancedMarker
+        ref={markerRef}
+        onClick={onMarkerClick}
+        position={{lat: city.position.lat, lng: city.position.lng}}
+        title={city.name}>
+      </AdvancedMarker>
+      {InfoWindowOpen && (
+        <InfoWindow
+          anchor={marker}
+          maxWidth={200}
+          onClose={handleClose}>
+          <div>{city.name}</div>
+        </InfoWindow>
+      )}
+    </>
+  )
+}
+
+
+
+// function MarkerWithInfoWindow() {
+//   const [infowindowOpen, setInfowindowOpen] = useState(true);
+//   const [markerRef, marker] = useAdvancedMarkerRef();
+//   return (
+//     <>
+//       <AdvancedMarker
+//         ref={markerRef}
+//         onClick={() => setInfowindowOpen(true)}
+//         position={{lat: 35.6917, lng: 139.75}}
+//         title={'AdvancedMarker that opens an Infowindow when clicked.'}
+//       >
+//         <Pin
+//           background={'#22ccff'}
+//           borderColor={'#1e89a1'}
+//           glyphColor={'#0f677a'}></Pin>
+//       </AdvancedMarker>
+//       {infowindowOpen && (
+//         <InfoWindow
+//           anchor={marker}
+//           maxWidth={200}
+//           onCloseClick={() => setInfowindowOpen(false)}>
+//           This is an example for the{' '}
+//           <code style={{whiteSpace: 'nowrap'}}>&lt;AdvancedMarker /&gt;</code>{' '}
+//           combined with an Infowindow.
+//         </InfoWindow>
+//       )}
+//     </>
+//   );
+// }
+
+
+//マーカーを描画するコンポーネント
+function CityMarkers() {
+  //モックデータ
+  const cityIdMapping = [
+    { id: 1, name: '東京', position: { lat: 35.6917, lng: 139.75, alt: 25.2 }},
+    { id: 2, name: '大阪', position: { lat: 34.6817, lng: 135.5183, alt: 23.0 }},
+    { id: 3, name: '札幌', position: { lat: 43.06, lng: 141.3283, alt: 17.4 }},
+    { id: 4, name: '那覇', position: { lat: 26.2067, lng: 127.6867, alt: 28.1 }},
+  ]
+
+  // const [selected, setSelected] = useState(null);
+
+  // const markerImage = '/images/marker.png'
+
+  return (
+    <>
+      {cityIdMapping.map((city) => (
+        <Marker 
+          key={city.id}
+          position={{ lat: city.position.lat, lng: city.position.lng }}
+          // icon={{
+          //   url: markerImage,
+          //   scaledSize: new window.google.maps.Size(25, 35),
+          // }}
+
+          onClick={() => {
+            console.log('city_id: ', city.id, 'city_name: ', city.name)
+          }}
+          // onMouseOver={() => {
+          //   setSelected(city);
+          // }}
+        />
+      ))}
+
+      {/* {selected ? (
+        <InfoWindow
+          // position={{ lat: selected.position.lat, lng: selected.position.lng }}
+          anchor={selected}
+          pixelOffset={new window.google.maps.Size(0, -10)}
+          onCloseClick={() => {
+            setSelected(null)
+          }}
+        >
+          <>
+            <div>id: {selected.id}</div>
+            <div>都市名: {selected.name}</div>
+          </>
+        </InfoWindow>
+      ) : null} */}
+    </>
+  );
+}
+
 
 // const libraries = ["places"];
 // const mapContainerStyle = {
