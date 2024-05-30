cb0becc42fad06148ce5272fa5947d9f093b5a25
g.sawada <sawada.gota@gmail.com>
2024-05-31 00:52:11 +0900

    createコントローラーの編集

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,14 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:email])
+    @user&.deliver_reset_password_instructions!
+    # 送信の失敗を通知すること自体にセキュリティリスクがあるため、失敗時にも強制的に処理を先へ進める
+
+    redirect_to login_path, info: "パスワード再設定用のメールを送信しました"
+  end
+
   def edit
   end
 end
