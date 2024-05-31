3c772ac6866d3670d4588041af64479f64e91553
g.sawada <sawada.gota@gmail.com>
2024-05-31 09:44:41 +0900

    パスワード再設定画面の実装。再設定の実行をローカルで動作確認

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,11 @@
 class PasswordResetsController < ApplicationController
+  # パスワード再設定申請画面の表示
+  # GET /password_resets/new
   def new
   end
 
+  # パスワード再設定用メール送信
+  # POST /password_resets
   def create
     @user = User.find_by(email: params[:email])
     @user&.deliver_reset_password_instructions!
@@ -10,6 +14,28 @@ class PasswordResetsController < ApplicationController
     redirect_to login_path, info: "パスワード再設定用のメールを送信しました"
   end
 
+  # パスワード再設定画面の表示
+  # GET /password_resets/:id/edit         // :id はトークン
   def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+
+    return not_authenticated if @user.blank?
+  end
+
+  # パスワード再設定処理
+  # PATCH /password_resets/:id
+  def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+
+    return not_authenticated if @user.blank?
+
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.change_password(params[:user][:password])
+      redirect_to login_path, success: "パスワードを変更しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 end
