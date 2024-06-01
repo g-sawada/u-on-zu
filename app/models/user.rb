e957f67b0bc2c1298e77ec916a9b250aa1895670
g.sawada <sawada.gota@gmail.com>
2024-06-01 12:22:52 +0900

    Userのoccupationを必須（presence: true）にしないよう変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :username,              presence: true, length: { maximum: 30 }
   validates :email,                 presence: true, uniqueness: true, length: { maximum: 255 }
-  validates :occupation,            presence: true
+  # validates :occupation,            presence: true
 
   enum occupation: {
     student_elementary: 0, student_junior: 1, student_high: 2, student_college: 3, teacher_elementary: 4,
