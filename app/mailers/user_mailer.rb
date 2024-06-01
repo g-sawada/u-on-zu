7ebee7fa82fffb30820bf8b7cad33ab8f5c0b49f
g.sawada <sawada.gota@gmail.com>
2024-06-01 09:09:59 +0900

    UserMailerの,抜け修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
     mail(
-      from: 'U-ON-ZU! お知らせメール <info@u-on-zu.com>'
+      from: 'U-ON-ZU! お知らせメール <info@u-on-zu.com>',
       to: user.email,
       subject: 'パスワード再設定のお手続き')
   end
