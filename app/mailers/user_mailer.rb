9abf6ce9fc9d94bcf92fe1f68a1ce2cba03076da
g.sawada <sawada.gota@gmail.com>
2024-06-01 10:08:50 +0900

    UserMailerのfrom設定を開発環境と本番環境で分岐するようにせってい

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'from@example.com'
-
   def reset_password_email(user)
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
     mail(
-      from: Rails.env.production? ? 'U-ON-ZU! お知らせメール <info@u-on-zu.com>' : nil,
+      from: Rails.env.production? ? 'U-ON-ZU! お知らせメール <info@u-on-zu.com>' : 'from@example.com',
       to: user.email,
       subject: 'パスワード再設定のお手続き')
   end
