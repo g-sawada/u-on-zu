c1e3a1da7aa8cf273cd8f59b6f87a9eccaea3541
g.sawada <sawada.gota@gmail.com>
2024-06-05 11:14:01 +0900

    プライバシーポリシーの作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,7 @@ class StaticPagesController < ApplicationController
 
   def terms_of_service
   end
+
+  def privacy_policy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "develop", to: "static_pages#develop"
   get "contact", to: "static_pages#contact"
   get "terms_of_service", to: "static_pages#terms_of_service"
+  get "privacy_policy", to: "static_pages#privacy_policy"
 
   # ユーザー関連
   resources :users, only: %i[new create destroy]
