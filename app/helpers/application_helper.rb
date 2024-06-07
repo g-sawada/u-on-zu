29cb61bf1f38f2048699c468dbb07761637587aa
g.sawada <sawada.gota@gmail.com>
2024-06-07 13:43:12 +0900

    フラッシュメッセージにアニメーションをつけ，cssを改善

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :info then "bg-info"
+    when :success then "bg-success"
+    when :warning  then "bg-warning"
+    when :error  then "bg-error"
+    else "bg-gray-500"
+    end
+  end
 end
