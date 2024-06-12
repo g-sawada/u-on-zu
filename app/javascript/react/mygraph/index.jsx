1212853524f6d1bfbeebc56f8088a15b9bab2be6
g.sawada <sawada.gota@gmail.com>
2024-06-12 09:05:25 +0900

    Graphコンポーネントの導入

--- app/javascript/react/mygraph/index.jsx
+++ app/javascript/react/mygraph/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Graph from '../canvas/components/graph/graph';
+
 
 export default function MyGraphApp() {
-  return <div>MyGraphApp</div>;
+  return (
+    <div className='flex justify-center items-center'>
+      <Graph data={graphInput} sv={settingValues}/>
+    </div>
+  )
 }
