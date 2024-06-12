85553d1e56353518562304b554ea2e065bbf0bc1
g.sawada <sawada.gota@gmail.com>
2024-06-12 23:40:13 +0900

    updateアクションの実装とturbo: render処理の追加

--- app/javascript/react/entrypoints/mygraph_entry.jsx
+++ app/javascript/react/entrypoints/mygraph_entry.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client';
 import MyGraphApp from '../mygraph';
 
 document.addEventListener('turbo:load', () => {
+  renderMyGraphApp();
+})
+
+document.addEventListener('turbo:render', () => {
+  renderMyGraphApp();
+})
+
+function renderMyGraphApp() {
   const container = document.getElementById('mygraph-app');
   if (container) {
+    console.log('mygraph-app found');
     const graphId = container.getAttribute('data-id');
     createRoot(container).render(<MyGraphApp graphId={graphId}/>);
   } else {
   console.log('mygraph-app not found');
   }
-})
+}
