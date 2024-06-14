73ec66e3ca7ef62a534386bc46b233f0d6418bfd
g.sawada <sawada.gota@gmail.com>
2024-06-14 12:11:02 +0900

    ディレクトリ構造とファイル名をデフォルトに戻す

--- app/uploaders/graph_thumbnail_uploader.rb
+++ app/uploaders/graph_thumbnail_uploader.rb
@@ -10,8 +10,8 @@ class GraphThumbnailUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -42,8 +42,8 @@ class GraphThumbnailUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    @random_string ||= SecureRandom.urlsafe_base64(8)
-    "#{original_filename.gsub('.png', '')}_#{@random_string}.png" if original_filename
-  end
+  # def filename
+  #   @random_string ||= SecureRandom.urlsafe_base64(8)
+  #   "#{original_filename.gsub('.png', '')}_#{@random_string}.png" if original_filename
+  # end
 end
