24e032056fc03c15683c2c78406f84235bbf64e0
g.sawada <sawada.gota@gmail.com>
2024-06-14 13:33:04 +0900

    AWS S3との連携確認

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'u-on-zu-bucket' # バケット名
+    config.fog_directory  = 'u-on-zu-bucket2' # バケット名
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
