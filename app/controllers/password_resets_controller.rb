1c6c7fbacef906619daf37f91c071585cac15660
g.sawada <sawada.gota@gmail.com>
2024-10-25 23:29:51 +0900

    password_resets_controllerのメール配信処理をCO

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
   # POST /password_resets
   def create
     @user = User.find_by(email: params[:email])
-    @user&.deliver_reset_password_instructions!
+    # @user&.deliver_reset_password_instructions!
     # 送信の失敗を通知すること自体にセキュリティリスクがあるため、失敗時にも強制的に処理を先へ進める
 
     redirect_to login_path, info: "パスワード再設定用のメールを送信しました"
